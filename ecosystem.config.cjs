21c1ee9b070dbc889624c567239820e657966d93
rohansingh4 <rohan.singh1104@gmail.com>
2025-12-04 07:51:19 +0000

    changes from aws

---
+++ ecosystem.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'surfliquid-velocity',
+      script: 'sonic-execution-onchain.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_file: './logs/combined.log',
+      time: true,
+    },
+  ],
+};
+
+
